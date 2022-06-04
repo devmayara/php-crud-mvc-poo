3fa3f18583263eaa22f1f7747c125a357f0d9cba
Mayara Silva <devmayara11@gmail.com>
2022-06-04 12:17:11 -0300

    realizada a conexao com o banco de dados

--- configuration/connect.php
+++ configuration/connect.php
@@ -1,1 +1,29 @@
 <!-- Conexão com o Banco de Dados -->
+<?php
+
+define('HOST','localhost');
+define('DATABASE','php-crud-mvc-poo');
+define('USER', 'root');
+define('PASSWORD', '');
+
+class Connect{
+    protected $connection;
+
+    function __construct()
+    {
+        $this->connectDatabase();
+    }
+
+    function connectDatabase()
+    {
+        try
+        {
+            $this->connection = new PDO('mysql:host='.HOST.';dbname='.DATABASE, USER, PASSWORD);
+        }
+        catch (PDOException $e)
+        {
+            echo "ERROR".$e->getMessage();
+            die();
+        }
+    }
+}
